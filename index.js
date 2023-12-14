4027e5d57e9e755a453268a9bdb79458467f33c8
tabitha muriithi <excellentwriter120@gmail.com>
2023-12-14 17:13:07 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
+let num1 = '31';
+let num2 = '2';
 
-// Write your code here
+
+    const multiply = (num1*num2);
+
+console.log(multiply);
+
+
+let numx= '2';
+
+    const random = (numx);
+
+console.log(numx)
+
+let num3 = '13'
+let num4 = '9'
+
+    const mod = (num3/num4);
+
+console.log(mod);
+
+
+
+const numberSet = [5, 3, 9, 20, 1];
+
+    const max = Math.max (...numberSet);
+
+console.log(max);
+
+
+
+
+
+
